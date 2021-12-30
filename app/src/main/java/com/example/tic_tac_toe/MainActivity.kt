bdbe76bc7796977ec660b1602c64a1882aecab90
luis <killuari@protonmail.com>
2021-12-30 01:35:45 +0100

    added game with difficulty easy
    - easy game is working
    - reset button is very buggy
    (resetted buttons cannot be overwritten after reset)

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -9,14 +9,24 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import com.example.tic_tac_toe.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var difficulty: Enum<Difficulty>
-    private lateinit var difficultyText: TextView
+    private lateinit var btns: MutableList<ImageButton>
+    private lateinit var availableBtns: MutableList<ImageButton>
+    private var btns_o: MutableList<ImageButton> = mutableListOf()
+    private var btns_x: MutableList<ImageButton> = mutableListOf()
+    private lateinit var wins_x: MutableList<Boolean>
+    private lateinit var wins_o: MutableList<Boolean>
+    private var won = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +35,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         difficulty = Difficulty.EASY
-        difficultyText = findViewById(R.id.difficulty)
-        difficultyText.text = difficulty.toString().lowercase()
+        binding.difficulty.text = difficulty.toString().lowercase()
             .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 
         binding.fab.setOnClickListener { view ->
@@ -34,6 +43,108 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
 
+        btns = mutableListOf(binding.imageButton1, binding.imageButton2, binding.imageButton3, binding.imageButton4,
+            binding.imageButton5, binding.imageButton6, binding.imageButton7, binding.imageButton8, binding.imageButton9)
+        availableBtns = btns
+
+        for(bt in btns) {
+            bt.setOnClickListener { clicked(it) }
+        }
+
+        binding.resetButton.setOnClickListener { reset() }
+
+    }
+
+    fun clicked(btn: View){
+        if(!won) {
+            if (difficulty == Difficulty.EASY) {
+                easyGame(btn)
+            }
+        }
+    }
+
+    private fun easyGame(btn: View): Boolean{
+        if(btn in availableBtns){
+            btn.setBackgroundResource(R.drawable.tic_tac_toe_o)
+            btns_o.add(btn as ImageButton)
+            availableBtns.remove(btn)
+
+            if(checkWin_o()){
+                Snackbar.make(binding.coordinatorLayout, "You have won!", Snackbar.LENGTH_LONG).show()
+                won = true
+                return true
+            }
+            if(availableBtns.size > 0) {
+                val randInt = (0 until availableBtns.size).random()
+                val btn_x = availableBtns[randInt]
+                btn_x.setBackgroundResource(R.drawable.tic_tac_toe_x)
+                btns_x.add(btn_x)
+                availableBtns.remove(btn_x)
+
+                if (checkWin_x()) {
+                    Snackbar.make(binding.coordinatorLayout, "The Computer has won!", Snackbar.LENGTH_LONG).show()
+                    won = true
+                    return true
+                }
+            }
+        }
+        return true
+    }
+
+
+    private fun reset() {
+//        for(bt in btns) {
+//            bt.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+//        }
+        binding.imageButton1.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton2.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton3.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton4.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton5.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton6.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton7.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton8.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+        binding.imageButton9.setBackgroundResource(R.drawable.tic_tac_toe_empty)
+
+        availableBtns = btns
+        btns_o = mutableListOf()
+        btns_x = mutableListOf()
+        won = false
+    }
+
+    private fun checkWin_x(): Boolean {
+        setWins()
+        for(win in wins_x) {
+            if(win){ return true }
+        }
+        return false
+    }
+
+    private fun checkWin_o(): Boolean {
+        setWins()
+        for(win in wins_o) {
+            if(win){ return true }
+        }
+        return false
+    }
+
+    private fun setWins(){
+        wins_x = mutableListOf(binding.imageButton1 in btns_x && binding.imageButton2 in btns_x && binding.imageButton3 in btns_x,
+            binding.imageButton4 in btns_x && binding.imageButton5 in btns_x && binding.imageButton6 in btns_x,
+            binding.imageButton7 in btns_x && binding.imageButton8 in btns_x && binding.imageButton9 in btns_x,
+            binding.imageButton1 in btns_x && binding.imageButton4 in btns_x && binding.imageButton7 in btns_x,
+            binding.imageButton2 in btns_x && binding.imageButton5 in btns_x && binding.imageButton8 in btns_x,
+            binding.imageButton3 in btns_x && binding.imageButton6 in btns_x && binding.imageButton9 in btns_x,
+            binding.imageButton1 in btns_x && binding.imageButton5 in btns_x && binding.imageButton9 in btns_x,
+            binding.imageButton3 in btns_x && binding.imageButton5 in btns_x && binding.imageButton7 in btns_x,)
+        wins_o = mutableListOf(binding.imageButton1 in btns_o && binding.imageButton2 in btns_o && binding.imageButton3 in btns_o,
+            binding.imageButton4 in btns_o && binding.imageButton5 in btns_o && binding.imageButton6 in btns_o,
+            binding.imageButton7 in btns_o && binding.imageButton8 in btns_o && binding.imageButton9 in btns_o,
+            binding.imageButton1 in btns_o && binding.imageButton4 in btns_o && binding.imageButton7 in btns_o,
+            binding.imageButton2 in btns_o && binding.imageButton5 in btns_o && binding.imageButton8 in btns_o,
+            binding.imageButton3 in btns_o && binding.imageButton6 in btns_o && binding.imageButton9 in btns_o,
+            binding.imageButton1 in btns_o && binding.imageButton5 in btns_o && binding.imageButton9 in btns_o,
+            binding.imageButton3 in btns_o && binding.imageButton5 in btns_o && binding.imageButton7 in btns_o,)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,21 +161,21 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             R.id.action_easy -> {
                 difficulty = Difficulty.EASY
-                difficultyText.text = difficulty.toString().lowercase()
+                binding.difficulty.text = difficulty.toString().lowercase()
                     .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
                 true
             }
 
             R.id.action_medium -> {
                 difficulty = Difficulty.MEDIUM
-                difficultyText.text = difficulty.toString().lowercase()
+                binding.difficulty.text = difficulty.toString().lowercase()
                     .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
                 true
             }
 
             R.id.action_impossible -> {
                 difficulty = Difficulty.IMPOSSIBLE
-                difficultyText.text = difficulty.toString().lowercase()
+                binding.difficulty.text = difficulty.toString().lowercase()
                     .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
                 true
             }
